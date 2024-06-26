a7cafb512cbed04f1e2478beeaf574d0bef3835a
Rajesh Tulluri <rajesh.tulluri@labor.alabama.gov>
2024-06-26 11:04:45 -0500

    Added Service and move properties list to properties.json file

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,53 +1,26 @@
-import { Component } from '@angular/core';
+
+
+import { Component,OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
+
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css']
 })
-export class PropertyListComponent {
-  properties:Array<any>=[
-    {
-    "Id":1,
-    "Type":"House",
-    "Price":1200,
-    "Name":"Birla House"
+export class PropertyListComponent implements OnInit{
+  properties:any;
+  constructor(private housingService: HousingService){
     
-    },
-    {
-        "Id":2,
-        "Type":"House",
-        "Price":1900,
-        "Name":"Nanda House"
-        
-    },
-    {
-            "Id":3,
-            "Type":"House",
-            "Price":1400,
-            "Name":"Tata House"
-            
-    },
-    {
-      "Id":4,
-      "Type":"House",
-      "Price":1100,
-      "Name":"Jagan House"
+  }
+    ngOnInit(): void
+    {this.housingService.getAllProperties().subscribe(data=>{
+       this.properties=data;
+       console.log(data)}, error=>{
+        console.log(error);
+       }
       
-    },
-   {
-          "Id":5,
-          "Type":"House",
-          "Price":1500,
-          "Name":"Kusmanch House"
-          
-    },
-    {
-      "Id":6,
-      "Type":"House",
-      "Price":1900,
-      "Name":"Tmp House"
-      
+      );
     }
-    ]
 }
