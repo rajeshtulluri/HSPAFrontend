731ce9554502ea00a67e9d045b387a064f000e1d
Rajesh Tulluri <rajesh.tulluri@labor.alabama.gov>
2024-07-08 10:25:44 -0500

    Implemented Routing All Parts

--- src/app/property/iProperty.interface.ts
+++ src/app/property/iProperty.interface.ts
@@ -1,6 +1,8 @@
 export interface IProperty{
     Id: number;
+    SellRent:Number,
     Name: string;
     Type: string;
     Price: number;
+    Image?:string;
 }
--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -3,6 +3,7 @@
 import { Component,OnInit } from '@angular/core';
 import { HousingService } from 'src/app/services/housing.service';
 import { IProperty } from '../iProperty.interface';
+import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -12,12 +13,19 @@ import { IProperty } from '../iProperty.interface';
 })
 export class PropertyListComponent implements OnInit{
   properties: Array<IProperty>=[];
-  constructor(private housingService: HousingService){
+  SellRent=1;
+  constructor(private housingService: HousingService, private route:ActivatedRoute){
     
   }
     ngOnInit(): void
-    {this.housingService.getAllProperties().subscribe(data=>{
+    { if(this.route.snapshot.url.toString())
+      {
+        this.SellRent=2;
+      }
+      this.housingService.getAllProperties(this.SellRent).subscribe(data=>{
        this.properties=data;
+       console.log(data);
+       console.log(this.route.snapshot.url.toString());
        console.log(data)}, error=>{
         console.log(error);
        }
